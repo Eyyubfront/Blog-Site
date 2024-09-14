b827114f3e452c2104b4d22c942dc02985197db9
itzjustfitb <fakhrigajar@gmail.com>
2024-09-15 00:17:45 +0400

    header and hero was finished

--- blog-website/src/App.jsx
+++ blog-website/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import Routing from './Routing';
+import React from "react";
+import Routing from "./Routing";
 import "./assets/styles/main.css";
+import Header from "./components/Header/Header";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <>
+      <Header />
       <Routing />
+      <Card />
     </>
   );
 }
