ed5824459061973520d46fe763c356a7c211cc23
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-09-14 13:15:47 -0700

    detail finish

--- blog-website/src/App.jsx
+++ blog-website/src/App.jsx
@@ -3,11 +3,13 @@ import Routing from './Routing';
 import "./assets/styles/main.css";
 import Footer from './components/Footer/Footer';
 import Subscription from './components/Subscription/Subscription';
+import Detail from './components/Detail/Detail';
 
 function App() {
   return (
     <>
       <Routing />
+      <Detail/>
       <Subscription/>
       <Footer/>
     </>
