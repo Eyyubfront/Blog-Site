29973379963d31191403c6cfea8129a40ad86c4b
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-09-14 12:46:27 -0700

    subscription card

--- blog-website/src/App.jsx
+++ blog-website/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Routing from './Routing';
 import "./assets/styles/main.css";
 import Footer from './components/Footer/Footer';
+import Subscription from './components/Subscription/Subscription';
 
 function App() {
   return (
     <>
       <Routing />
+      <Subscription/>
       <Footer/>
     </>
   );
--- blog-website/src/Routing/index.jsx
+++ blog-website/src/Routing/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 
+
 const Routing = () => {
   return (
     <Routes>
-
     </Routes>
   );
 };
