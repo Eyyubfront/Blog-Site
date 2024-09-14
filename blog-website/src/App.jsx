1fae457147edbe811963c9b0fa12bc5c49f4b3ce
itzjustfitb <fakhrigajar@gmail.com>
2024-09-15 01:51:45 +0400

    page section was added

--- blog-website/src/App.jsx
+++ blog-website/src/App.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 import Routing from "./Routing";
 import "./assets/styles/main.css";
-
 import Header from "./components/Header/Header";
-import Card from "./components/Card/Card";
-
-import Footer from './components/Footer/Footer';
-import Subscription from './components/Subscription/Subscription';
-import Detail from './components/Detail/Detail';
-
+import Footer from "./components/Footer/Footer";
+import Subscription from "./components/Subscription/Subscription";
 
 function App() {
   return (
     <>
       <Header />
       <Routing />
-
-      <Card />
-
-      <Detail/>
-      <Subscription/>
-      <Footer/>
-
+      <Subscription />
+      <Footer />
     </>
   );
 }
