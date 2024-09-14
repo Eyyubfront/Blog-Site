57fd1861ad13cef5ae825798c0c0b6d7c9971d14
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-09-14 11:47:49 -0700

    start website

---
+++ blog-website/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routing from './Routing';
+import "./assets/styles/main.css";
+
+function App() {
+  return (
+    <>
+      <Routing />
+    </>
+  );
+}
+export default App;
