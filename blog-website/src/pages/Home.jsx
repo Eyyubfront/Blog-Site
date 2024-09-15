782519decead47af52271ac206928de238ae0fec
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-09-14 17:34:24 -0700

    eyyub

--- blog-website/src/pages/Home.jsx
+++ blog-website/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Hero from "../components/Hero/Hero";
 import PageSection from "../components/PageSection/PageSection";
 
 function Home() {
-  const [blogs,setBlogs]= Usestate()
   return (
     <main id="home">
       <Hero />
